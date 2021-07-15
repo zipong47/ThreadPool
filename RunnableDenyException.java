d97abf1ac5514f31f896f0ab5a1b4356a6edb2bb
zipong <39720650+zipong47@users.noreply.github.com>
2021-07-15 17:30:52 +0800

    上传代码

---
+++ RunnableDenyException.java
@@ -0,0 +1,7 @@
+package phase1.chapter8;
+
+public class RunnableDenyException extends RuntimeException {
+    public RunnableDenyException(String message) {
+        super(message);
+    }
+}
---
+++ ThreadFactory.java
@@ -0,0 +1,9 @@
+package phase1.chapter8;
+
+/**
+ * 提供创建线程的接口 来个性化地定制Thread
+ */
+@FunctionalInterface
+public interface ThreadFactory {
+    Thread createThread(Runnable runnable);
+}
